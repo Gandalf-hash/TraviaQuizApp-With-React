b3a560fb4ab64dabe880b0e7a37330ea38f9aa0c
Johannes <phiritshepiso99@gmail.com>
2023-03-28 14:44:40 +0200

    Completed first task

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import "./Footer.css";
+import React from 'react'
+
+function Footer() {
+  return <footer className="footer">© J Phetoane, 2023</footer>
+   
+
+}
+
+export default Footer;
---
+++ src/trivia-item.js
@@ -0,0 +1,124 @@
+const triviaItems = [
+    {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "The board game &quot;Monopoly&quot; is a variation of what board game?",
+        "correct_answer": "The Landlord&#039;s Game",
+        "incorrect_answers": [
+        "Territorial Dispute",
+        "Property Feud",
+        "The Monopolist&#039;s Game"
+        ]
+        },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Carcassonne is based on which French town?",
+        "correct_answer": "Carcassonne",
+        "incorrect_answers": [
+        "Paris",
+        "Marseille",
+        "Clermont-Ferrand"
+        ]
+        },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "The board game, Nightmare was released in what year?",
+        "correct_answer": "1991",
+        "incorrect_answers": [
+        "1992",
+        "1989",
+        "1995"
+        ]
+        },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "How many points is the Z tile worth in Scrabble?",
+        "correct_answer": "10",
+        "incorrect_answers": [
+        "8",
+        "5",
+        "6"
+        ]
+        },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Which of these games includes the phrase &quot;Do not pass Go, do not collect $200&quot;?",
+        "correct_answer": "Monopoly",
+        "incorrect_answers": [
+        "Pay Day",
+        "Cluedo",
+        "Coppit"
+        ]
+        },
+        {
+            "category": "General Knowledge",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What is the romanized Russian word for &quot;winter&quot;?",
+            "correct_answer": "Zima",
+            "incorrect_answers": [
+            "Leto",
+            "Vesna",
+            "Osen&#039;"
+            ]
+            },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "What is the most challenging monster in the Dungeons &amp; Dragons 5th Edition Monster Manual?",
+        "correct_answer": "Tarrasque",
+        "incorrect_answers": [
+        "Beholder",
+        "Displacer Beast",
+        "Lich"
+        ]
+        },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "Which ability from the &quot;Magic: The Gathering&quot; Scars of Mirrodin expansion involves having at least three artifacts in play?",
+        "correct_answer": "Metalcraft",
+        "incorrect_answers": [
+        "Affinity",
+        "Imprint",
+        "Living Weapon"
+        ]
+        },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "Some of the &quot;Fallen Empires&quot; cards from &quot;Magic: The Gathering&quot; were misprinted on the backs of which other card game?",
+        "correct_answer": "Wyvern",
+        "incorrect_answers": [
+        "Pokemon",
+        "Dominion",
+        "Yu-Gi-Oh"
+        ]
+        },
+        {
+        "category": "Entertainment: Board Games",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "What is the sum of all the tiles in a standard box of Scrabble?",
+        "correct_answer": "187",
+        "incorrect_answers": [
+        "207",
+        "197",
+        "177"
+        ]
+        }
+]
+
+export default triviaItems;
