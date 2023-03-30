6d24b81adef2ca1ec76abba0be1a5d5a7f0c8dfa
Johannes <phiritshepiso99@gmail.com>
2023-03-30 13:29:43 +0200

    Added Loading Spinner

--- src/QuizPage.js
+++ src/QuizPage.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Game from './Game';
 import TriviaItem from './TriviaItem';
+import LoadingSpinner from './common/loading-spinner';
+import ErrorMessage from './common/error-message';
 
 
 function QuizPage() {
+  const [quizFetch, setQuizFetch] = useState({
+    isLoading: true,
+    errorMessage: "",
+    data: null,
+  });
+
+  const {isLoading, errorMessage, data} = quizFetch;
+
+  let contents;
+  if (isLoading)
+    contents = <LoadingSpinner/>;
+  else if (errorMessage !== "")
+    contents = <ErrorMessage>{errorMessage}</ErrorMessage>;
+  else
+    contents = <Game/>;
  
     return (
-      <>
-        <Game />
-        <TriviaItem />
-        
-      </>
+      <main>
+        {/* {contents} */}
+      </main>
     )
   
 }
--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ import EndScreen from './Endscreen';
 function  App()  {
     return (
       <>
-      <Game />
+      <QuizPage/>
+      {/* <Game /> */}
 
 
 
